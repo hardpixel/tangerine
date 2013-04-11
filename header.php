f321df1c6483742d73745d01380e058d01d30ea7
Olibia Tsati <olibia@hardpixel.net>
2013-04-10 17:53:39 -0700

    Added options for blog title

--- header.php
+++ header.php
@@ -31,7 +31,10 @@
 					<?php if( get_theme_mod('header_image') != '' ): ?>
 						<img src="<?php echo get_theme_mod('header_image'); ?>" alt="" />
 					<?php endif; ?>
-					<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="name"><?php bloginfo( 'name' ); ?></a> <small class="description"><?php bloginfo( 'description' ); ?></small></h1>
+
+					<?php if( get_theme_mod('header_title') == '1' ): ?>
+						<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="name"><?php bloginfo( 'name' ); ?></a> <small class="description"><?php bloginfo( 'description' ); ?></small></h1>
+					<?php endif; ?>
 				</div>
 
 			</div>
