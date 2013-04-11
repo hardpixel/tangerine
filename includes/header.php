9296bbd2f3fdfdcd7db5b8e38b932d585ea0c03d
Olibia Tsati <olibia@hardpixel.net>
2013-04-11 10:17:52 -0700

    Fixed header

--- includes/header.php
+++ includes/header.php
@@ -2,20 +2,21 @@
 
 	function tangerine_header(){
 	?>
-		<div id="header">
+		<?php if( get_theme_mod( 'header_image' ) != '' || get_theme_mod('header_title') == '1' ); ?>
+			<div id="header">
 
-			<div class="header-inner">
-				<?php if( get_theme_mod('header_image') != '' ): ?>
-					<img src="<?php echo get_theme_mod('header_image'); ?>" alt="" />
-				<?php endif; ?>
+				<div class="header-inner">
+					<?php if( get_theme_mod('header_image') != '' ): ?>
+						<img src="<?php echo get_theme_mod('header_image'); ?>" alt="" />
+					<?php endif; ?>
 
-				<?php if( get_theme_mod('header_title') == '1' ): ?>
-					<h1 class="name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="name"><?php bloginfo( 'name' ); ?></a> <small class="description"><?php bloginfo( 'description' ); ?></small></h1>
-				<?php endif; ?>
+					<?php if( get_theme_mod('header_title') == '1' ): ?>
+						<h1 class="name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="name"><?php bloginfo( 'name' ); ?></a> <small class="description"><?php bloginfo( 'description' ); ?></small></h1>
+					<?php endif; ?>
+				</div>
+
 			</div>
-
-		</div>
-
+		<?php endif; ?>
 	<?php }
 
 ?>
