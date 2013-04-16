3978e6f67a005ba2005b3bf8ccf6dac959d24502
Olibia Tsati <olibia@hardpixel.net>
2013-04-16 04:44:33 -0700

    Small fix for child theme styles

--- index.php
+++ index.php
@@ -26,16 +26,20 @@
 					<p class="date round secondary label">Posted on: <span class="time"><?php echo get_the_date(); ?></span> by: <span class="author"><?php echo get_the_author(); ?></span></p>
 
 					<div class="excerpt">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<a id="link-<?php the_ID(); ?>" class="th featured" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_post_thumbnail( 'thumbnail' ); ?>
+							</a>
+						<?php endif; ?>
 						<?php the_excerpt(); ?>
 					</div>
 				<?php endif; ?>
 
 			</article>
-		<?php if( is_single() ) { echo '<hr>'; } ?>
 
 		<?php endwhile; endif; ?>
 
-		<?php if( !is_single() && !is_page() ) { pagination(); } ?>
+		<?php if( !is_single() && !is_page() ) { echo '<hr>'; pagination(); } ?>
 
 	</section>
 	<!-- END #content -->
