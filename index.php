05fbb6c811b55bd2a6e85e67a816cc0d37e1221f
Jonian Guveli <jonian@hardpixel.net>
2013-04-23 21:36:57 -0700

    Bug fixes

--- index.php
+++ index.php
@@ -4,44 +4,67 @@
 	<?php get_sidebar(); ?>
 
 	<section id="content" class="small-12 <?php dynamic_content_styles(); ?> columns">
+		
+		<?php if( is_page() ): ?>
 
-		<?php if ( !is_front_page() && is_page() && get_theme_mod( 'show_breadcrumbs' ) == '1' ) { breadcrumbs(); } ?>
-
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php if ( !is_front_page() && get_theme_mod( 'show_breadcrumbs' ) == '1' ) { breadcrumbs(); } ?>
 
 			<article class="<?php echo get_post_type( $post ); ?>">
-
-				<?php if ( is_single() || is_page() ): ?>
-					<h2><?php the_title(); ?></h2>
-
-					<?php if ( is_single() ): ?>
-						<p class="date round secondary label">Posted on: <span class="time"><?php echo get_the_date(); ?></span> by: <span class="author"><?php echo get_the_author(); ?></span></p>
-					<?php endif; ?>
-
-					<div class="details">
-						<?php the_content(); ?>
-					</div>
-				<?php else: ?>
-					<h2><a href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-					<p class="date round secondary label">Posted on: <span class="time"><?php echo get_the_date(); ?></span> by: <span class="author"><?php echo get_the_author(); ?></span></p>
-
-					<div class="excerpt">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<a id="link-<?php the_ID(); ?>" class="th featured" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>">
-								<?php the_post_thumbnail( 'thumbnail' ); ?>
-							</a>
-						<?php endif; ?>
-						<?php the_excerpt(); ?>
-					</div>
-				<?php endif; ?>
-
-				<?php if ( is_single() ) { comments_template(); } ?>
+				
+				<h2><?php the_title(); ?></h2>
+				<div class="details">
+					<?php the_content(); ?>
+				</div>
 
 			</article>
 
-		<?php endwhile; endif; ?>
+		<?php else: ?>
 
-		<?php if ( !is_single() && !is_page() ) { echo '<hr>'; pagination(); } ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<!-- post -->
+				
+				<article class="<?php echo get_post_type( $post ); ?>">
+
+					<?php if ( is_single() ): ?>
+						
+						<h2><?php the_title(); ?></h2>
+						<p class="date round secondary label">Posted on: <span class="time"><?php echo get_the_date(); ?></span> by: <span class="author"><?php echo get_the_author(); ?></span></p>
+
+						<div class="details">
+							<?php the_content(); ?>
+						</div>
+
+						<?php comments_template(); ?>
+
+					<?php else: ?>
+						
+						<h2><a href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<p class="date round secondary label">Posted on: <span class="time"><?php echo get_the_date(); ?></span> by: <span class="author"><?php echo get_the_author(); ?></span></p>
+
+						<div class="excerpt">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<a id="link-<?php the_ID(); ?>" class="th featured" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>">
+									<?php the_post_thumbnail( 'thumbnail' ); ?>
+								</a>
+							<?php endif; ?>
+							<?php the_excerpt(); ?>
+						</div>
+
+					<?php endif; ?>
+
+				</article>
+
+			<?php endwhile; ?>
+			<!-- post navigation -->
+				<hr>
+				<?php pagination(); ?>
+
+			<?php else: ?>
+			<!-- no posts found -->
+			<?php endif; ?>
+
+		<?php endif; ?>
+		
 
 	</section>
 	<!-- END #content -->
